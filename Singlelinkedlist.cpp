05e455c2499b5a79d3106fd340ecf3c5cb93f5c0
Bendadiotulada-323 <bendadiotulada@gmail.com>
2026-04-24 16:02:29 +0700

    add bool delnode

--- Singlelinkedlist.cpp
+++ Singlelinkedlist.cpp
@@ -74,4 +74,19 @@ void addnode()
         return (*current != NULL);   
     }
 
+   bool delnode(int nim)
+    {
+        node *current, *previous;
+        if (!search (nim, &previous, &current))
+            return false;
+
+        if (current == START)
+            START = START->next;
+         else
+            previous->next = current->next;
+
+        delete current;
+        return true;
+    }
+
    
