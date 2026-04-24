8ddf55c2135d79f17e1001f5a4ed55ff6bb4962d
Bendadiotulada-323 <bendadiotulada@gmail.com>
2026-04-24 15:59:47 +0700

    add void addnode

--- Singlelinkedlist.cpp
+++ Singlelinkedlist.cpp
@@ -16,3 +16,29 @@ linkedlist()
     {
         START = NULL;
     }
+
+void addnode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa";
+        cin >> nim;
+    
+        node *nodebaru = new node;
+        nodebaru->noMhs = nim;
+
+        if (START == NULL || nim <= START ->noMhs)
+        {
+            if ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi nomhs tidak diijinkan|n";
+                return;
+            }
+            nodebaru->next = START;
+            START = nodebaru;
+            return;
+        }
+
+        node *previous = START;
+        node *current = START;
+
+   
